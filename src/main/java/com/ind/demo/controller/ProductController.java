a717e8ee9d32e416273403b14444f448867076c7
Indra Kumar <indra2010.kumar@gmail.com>
2025-05-14 13:40:23 +0530

    Logger added

--- src/main/java/com/ind/demo/controller/ProductController.java
+++ src/main/java/com/ind/demo/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.ind.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
 
+    Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     @GetMapping("/{id}")
     public String findById(@PathVariable int id) {
+        logger.info(id + "jsdjashdjadhka");
         return "Product with id " + id + " found";
     }
 }
