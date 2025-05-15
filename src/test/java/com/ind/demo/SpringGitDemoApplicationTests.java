c9709821ae0aab3c0317fb224e5e08c78cd63115
Indra Kumar <indra2010.kumar@gmail.com>
2025-05-15 17:54:21 +0530

    test case logger added

--- src/test/java/com/ind/demo/SpringGitDemoApplicationTests.java
+++ src/test/java/com/ind/demo/SpringGitDemoApplicationTests.java
@@ -3,11 +3,18 @@ package com.ind.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SpringGitDemoApplicationTests {
 
+    Logger logger = Logger.getLogger("SpringGitDemoApplicationTests");
     @Test
     void contextLoads() {
+        logger.info("Hello World Test case executed::::");
+        assertEquals(true,true);
     }
 
 }
